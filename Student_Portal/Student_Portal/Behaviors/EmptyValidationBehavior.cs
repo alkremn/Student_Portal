3c0a8bfaaee54ed3d5915f090e1a0f277109715c
alkremn <alkremn@gmail.com>
2019-05-17 22:35:31 -0600

    Added empty behavior

---
+++ Student_Portal/Student_Portal/Behaviors/EmptyValidationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Student_Portal.Behaviors
+{
+    class EmptyValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool isValid = e.NewTextValue.Length != 0;
+            Entry entry = sender as Entry;
+            entry.PlaceholderColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
